28b9cec9472a39f63252d9162e901f9eff8506fd
jeandede <jeandelcour@hotmail.com>
2019-10-15 07:24:58 +0200

    Update fr.jobs.sql

--- fr.jobs.sql
+++ fr.jobs.sql
@@ -1,3 +1,8 @@
 INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
 ('busdriver', 'chauffeur de bus', 1)
 ;
+
+
+INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+('busdriver', 0, 'recruit', 'Recrue', 300, '{}', '{}')
+;
